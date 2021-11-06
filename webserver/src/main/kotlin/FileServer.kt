64ace4a152bc2311aac56df8496ef0ae4ef22efa
Исламбек Туранов <islambek198@gmail.com>
2021-11-06 14:45:58 +0300

    "Webserver done"

--- webserver/src/main/kotlin/FileServer.kt
+++ webserver/src/main/kotlin/FileServer.kt
@@ -1,4 +1,5 @@
 import ru.sber.filesystem.VFilesystem
+import ru.sber.filesystem.VPath
 import java.io.IOException
 import java.net.ServerSocket
 
@@ -7,6 +8,9 @@ import java.net.ServerSocket
  * requests from HTTP clients.
  */
 class FileServer {
+    private val notFound = "HTTP/1.0 404 Not Found\r\n\nServer: FileServer\r\n\n\r\n"
+    private val success = "HTTP/1.0 200 OK\r\nServer: FileServer\r\n\r\n"
+    private val goodResponseEnd = "\r\n"
 
     /**
      * Main entrypoint for the basic file server.
@@ -27,44 +31,38 @@ class FileServer {
          * ServerSocket object.
          */
         while (true) {
+            socket.use {
+                while (true) {
+                    it.accept().use { socket ->
+                        val reader = socket.getInputStream().bufferedReader()
+                        val request = reader.readLine().split(" ")
+                        val writer = socket.getOutputStream().bufferedWriter()
+                        val path = VPath(request[1])
+                        val info = fs.readFile(path)
 
-            // TODO Delete this once you start working on your solution.
-            //throw new UnsupportedOperationException();
-
-            // TODO 1) Use socket.accept to get a Socket object
-
-
-            /*
-            * TODO 2) Using Socket.getInputStream(), parse the received HTTP
-            * packet. In particular, we are interested in confirming this
-            * message is a GET and parsing out the path to the file we are
-            * GETing. Recall that for GET HTTP packets, the first line of the
-            * received packet will look something like:
-            *
-            *     GET /path/to/file HTTP/1.1
-            */
-
-
-            /*
-             * TODO 3) Using the parsed path to the target file, construct an
-             * HTTP reply and write it to Socket.getOutputStream(). If the file
-             * exists, the HTTP reply should be formatted as follows:
-             *
-             *   HTTP/1.0 200 OK\r\n
-             *   Server: FileServer\r\n
-             *   \r\n
-             *   FILE CONTENTS HERE\r\n
-             *
-             * If the specified file does not exist, you should return a reply
-             * with an error code 404 Not Found. This reply should be formatted
-             * as:
-             *
-             *   HTTP/1.0 404 Not Found\r\n
-             *   Server: FileServer\r\n
-             *   \r\n
-             *
-             * Don't forget to close the output stream.
-             */
+                        if (info.isNullOrEmpty()) {
+                            writer.write(HTTP.NOT_FOUND.answer)
+                        } else {
+                            writer.write(HTTP.OK.answer + "$info\r\n")
+                        }
+                        writer.flush()
+                        writer.close()
+                    }
+                }
+            }
         }
+    }
+
+    enum class HTTP(val answer: String) {
+        NOT_FOUND(
+            "HTTP/1.0 404 Not Found\r\n" +
+                    "Server: FileServer\r\n" +
+                    "\r\n"
+        ),
+        OK(
+            "HTTP/1.0 200 OK\r\n" +
+                    "Server: FileServer\r\n" +
+                    "\r\n"
+        )
     }
 }
